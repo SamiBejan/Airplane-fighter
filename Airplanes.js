42d75058171bcd9d9056b795d292f5939aead55d
Samuel Bejan <110773742+SamiBejan@users.noreply.github.com>
2023-06-22 19:06:48 +0300

    Update Airplanes.js
    Created the airplane and implemented the movement functionality.

--- Airplanes.js
+++ Airplanes.js
@@ -1,7 +1,9 @@
 const area = document.querySelector(".area");
 let grid = new Array(21);
+let airplane = [{"line": 2, "col": 10}, {"line": 1, "col": 9}, {"line": 1, "col": 10}, {"line": 1, "col": 11}];
 
 createSpace();
+createAirplane();
 
 function createSpace() {
     for (let i = 1; i < grid.length; ++i) {
@@ -13,6 +15,49 @@ function createSpace() {
             grid[i][j] = document.createElement("td");
             row.appendChild(grid[i][j]);
         }
-        area.appendChild(row);
+        area.prepend(row);
     }
 }
+
+function createAirplane() {
+    for (let i = 0; i < airplane.length; ++i) {
+        grid[airplane[i].line][airplane[i].col].classList.add("airplane");
+    }
+} 
+
+function move(e) {
+    if (isArrowKey(e.key) && !hitWall(e.key)) {
+        for (let i = 0; i < airplane.length; ++i) {
+            grid[airplane[i].line][airplane[i].col].classList.remove("airplane");
+        }
+        if (e.key === "ArrowUp") {
+            for (let i = 0; i < airplane.length; ++i) {
+                ++airplane[i].line;
+            }
+        } else if (e.key === "ArrowDown") {
+            for (let i = 0; i < airplane.length; ++i) {
+                --airplane[i].line;
+            }
+        } else if (e.key === "ArrowLeft") {
+            for (let i = 0; i < airplane.length; ++i) {
+                --airplane[i].col;
+            }
+        } else {
+            for (let i = 0; i < airplane.length; ++i) {
+                ++airplane[i].col;
+            }
+        }
+        createAirplane();
+    }
+}
+
+function isArrowKey(key) {
+    return (key === "ArrowUp" || key === "ArrowDown" || key === "ArrowLeft" || key === "ArrowRight");
+}
+
+function hitWall(key) {
+    return ((key === "ArrowUp" && airplane[0].line + 1 === 11) || (key === "ArrowDown" && airplane[2].line - 1 === 0) ||
+    (key === "ArrowLeft" && airplane[1].col - 1 === 0) || (key === "ArrowRight" && airplane[3].col + 1 === 21));
+}
+
+window.addEventListener("keydown", move);
